bb17346e865b183a38b9aaa3081578faa3730dda
Anton Lindström <lindztr@gmail.com>
2012-11-09 08:58:11 +0100

    add spec for mysql backend class

---
+++ spec/classes/powerdns__mysql_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe 'powerdns::mysql', :type => :class do
+
+  it do
+    should contain_package('pdns-backend-mysql').with_ensure('present')
+  end
+
+  it do
+    should contain_file('/etc/powerdns/pdns.d/pdns.local.mysql').with_ensure('present')
+  end
+
+end
