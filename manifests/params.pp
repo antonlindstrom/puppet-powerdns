afd51b786de9cdd0c4ad406b619533d6b585a635
Henrik <henrik@haf.se>
2012-11-25 15:54:29 +0100

    adding support for amazon linux

--- manifests/params.pp
+++ manifests/params.pp
@@ -7,32 +7,32 @@
 class powerdns::params {
 
   $package = $::operatingsystem ? {
-    /(?i:centos|redhat)/ => 'pdns',
+    /(?i:centos|redhat|amazon)/ => 'pdns',
     default              => 'pdns-server'
   }
 
   $package_provider = $::operatingsystem ? {
-    /(?i:centos|redhat)/  => 'rpm',
+    /(?i:centos|redhat|amazon)/  => 'rpm',
     default               => 'dpkg'
   }
 
   $package_psql = $::operatingsystem ? {
-    /(?i:centos|redhat)/ => 'pdns-backend-postgresql',
+    /(?i:centos|redhat|amazon)/ => 'pdns-backend-postgresql',
     default              => 'pdns-backend-pgsql'
   }
 
   $package_mysql = $::operatingsystem ? {
-    /(?i:centos|redhat)/ => 'pdns-backend-mysql',
+    /(?i:centos|redhat|amazon)/ => 'pdns-backend-mysql',
     default              => 'pdns-backend-mysql'
   }
 
   $postgresql_cfg_path = $::operatingsystem ? {
-    /(?i:centos|redhat)/ => '/etc/pdns/pdns.conf',
+    /(?i:centos|redhat|amazon)/ => '/etc/pdns/pdns.conf',
     default              => '/etc/powerdns/pdns.d/pdns.local.gpgsql'
   }
 
   $mysql_cfg_path = $::operatingsystem ? {
-    /(?i:centos|redhat)/ => '/etc/pdns/pdns.conf',
+    /(?i:centos|redhat|amazon)/ => '/etc/pdns/pdns.conf',
     default              => '/etc/powerdns/pdns.d/pdns.local.mysql'
   }
 
