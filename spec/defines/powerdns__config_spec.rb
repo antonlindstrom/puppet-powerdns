ec7f6281ff4bba4cfc89304919aff616787d3f85
Harm Jan Stam <h.j.stam@oxilion.nl>
2014-06-02 14:19:43 +0200

    Update test

--- spec/defines/powerdns__config_spec.rb
+++ spec/defines/powerdns__config_spec.rb
@@ -4,7 +4,8 @@ describe 'powerdns::config', :type => :define do
   let(:title) { 'cache-ttl' }
   let(:params) { { :value => '20' } }
 
-  it do should contain_file('/etc/powerdns/pdns.d/cache-ttl.conf').with(
+  it do should contain_file('cache-ttl.conf').with(
+
     'ensure'  => 'present',
     'owner'   => 'root',
     'group'   => 'root',
