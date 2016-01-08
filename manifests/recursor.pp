83d470c844a897ec27a31148797e036559b1df3d
jjolet <John Jolet>
2016-01-08 08:43:27 -0600

    added recursor_cfg_path to paramaters at the top of recursor.pp

--- manifests/recursor.pp
+++ manifests/recursor.pp
@@ -25,6 +25,7 @@
 #
 class powerdns::recursor(
   $package               = $powerdns::params::package_recursor,
+  $recursor_cfg_path     = $powerdns::params::recursor_cfg_path,
   $ensure                = 'present',
   $source                = '',
   $forward_zones         = undef,
